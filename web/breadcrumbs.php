146145b8874f1d4655cfba3a5a0d2375b2b905e8
FranciscoBarbosa <franciscolamares@gmail.com>
2017-02-23 17:16:17 +0000

    duplicated html removed

---
+++ web/breadcrumbs.php
@@ -0,0 +1,8 @@
+<div class="breadcrumbs">
+  <div class="container">
+    <ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
+      <li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Home</a></li>
+      <li class="active">About</li>
+    </ol>
+  </div>
+</div>
